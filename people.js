b016257693d535b2307f51cff053ea4dc138577f
PRAFUL KODIYATAR <61209229+prafulkodiyatar@users.noreply.github.com>
2022-07-09 12:48:26 +0530

    Add files via upload

---
+++ people.js
@@ -0,0 +1,8 @@
+const people=['praful','vijay','parbat','manish','amit','naran'];
+const ages=[20,24,22,25,21,20]
+
+console.log(people)
+
+module.exports = {
+    people,ages
+};
---
+++ try.js
@@ -0,0 +1,12 @@
+// function sum(a,b){
+//     return a+b;
+// }
+// console.log(sum(10,20));
+
+myFriend = ['praful','paresh','ramesh','raj'];
+console.log(myFriend.indexOf('paresh',2));
+
+myFriend.forEach(element=>{
+    console.log(element);
+})
+
